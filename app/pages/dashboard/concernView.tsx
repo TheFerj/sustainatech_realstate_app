deadc1bbc96543e3026b25d7bd6f99477d547cba
Dav <ferjendavetorred@gmail.com>
2023-05-23 11:12:34 +0800

    Edit concern view

--- app/pages/dashboard/concernView.tsx
+++ app/pages/dashboard/concernView.tsx
@@ -1,3 +1,4 @@
+
 'use client'
 
 import { useState } from 'react';
@@ -5,79 +6,113 @@ import { Button } from '../../../components/ui/button';
 
 interface ViewConcernFormProps {
   id: string;
-  concern:string;
-  title:string;
-  type:string;
-  urgency:string;
-  createdAt:string;
+  concern: string;
+  title: string;
+  type: string;
+  urgency: string;
+  createdAt: string;
 }
 
-export const ViewConcern: React.FC<ViewConcernFormProps> =  ({ id,concern,title,createdAt,type,urgency }) => {
-    async function getUserPosts() {
-        const res = await fetch('http://localhost:3000/api/user/' + id + '/post/userPost', {
-          method: 'GET',
-          headers: {
-            'Cache-Control': 'no-cache' // or other cache control directives
-          }
-        });
-      
-        if (!res.ok) {
-          throw new Error('Failed to fetch data');
-        }
-      
-        return res.json();
-      }
-      
-  
+export const ViewConcern: React.FC<ViewConcernFormProps> = ({
+  id,
+  concern,
+  title,
+  createdAt,
+  type,
+  urgency,
+}) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  async function getUserPosts() {
+    const res = await fetch('http://localhost:3000/api/user/' + id + '/post/userPost', {
+      method: 'GET',
+      headers: {
+        'Cache-Control': 'no-cache', // or other cache control directives
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  }
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
-        
-<div className="relative overflow-x-auto">
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" className="px-6 py-3">
-                    Title
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Concern
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Date
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Type
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Urgency
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Status
-                </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Concern
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Type
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Urgency
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody onClick={toggleModal} className={isClicked ? "cursor-pointer bg-gray-200" : "cursor-pointer"}>
             <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {title}
-                </th>
-                <td className="px-6 py-4">
-                    {concern}
-                </td>
-                <td className="px-6 py-4">
-                    {createdAt}
-                </td>
-                <td className="px-6 py-4">
-                    {type}
-                </td>
-                <td className="px-6 py-4">
-                    {urgency}
-                </td>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {title}
+              </th>
+              <td className="px-6 py-4">{concern}</td>
+              <td className="px-6 py-4">{createdAt}</td>
+              <td className="px-6 py-4">{type}</td>
+              <td className="px-6 py-4">{urgency}</td>
             </tr>
-        </tbody>
-    </table>
-</div>
-
+          </tbody>
+        </table>
+      </div>
+      {isModalOpen && (
+        <div
+          id="staticModal"
+          data-modal-backdrop="static"
+          aria-hidden="true"
+          className="fixed top-0 left-0 right-0 z-50 w-full h-screen flex items-center justify-center"
+        >
+          <div className="relative bg-white rounded-lg shadow p-6">
+            <h3 className="text-xl font-semibold mb-4">Details</h3>
+            <h3 className="text-l font-semibold mb-4">Date: {createdAt}</h3>
+            <h3 className="text-l font-semibold mb-4">Title: {title}</h3>
+            <h3 className="text-l font-semibold mb-4">Concern: {concern}</h3>
+            <h3 className="text-l font-semibold mb-4">Urgency: {urgency}</h3>
+            <h3 className="text-l font-semibold mb-4">Type: {type}</h3>
+            <h3 className="text-l font-semibold mb-4">Status: </h3>
+            <h3 className="text-l font-semibold mb-4">Comment/Remarks: </h3>
+            <div className="flex justify-end">
+              <button
+                type="button"
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10"
+                onClick={toggleModal}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>)}
     </>
   );
 };
