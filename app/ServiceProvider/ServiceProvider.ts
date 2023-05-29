effa9dfd400034e314cc2035b368001413b4ea50
Kyle Dominic Lomigo <91366618+KyleDom@users.noreply.github.com>
2023-05-29 08:55:17 +0800

    Update ServiceProvider.ts

--- app/ServiceProvider/ServiceProvider.ts
+++ app/ServiceProvider/ServiceProvider.ts
@@ -1,39 +1,24 @@
-import { EnergyTracker } from "@prisma/client";
 import { AppointmentManager } from "../CoreServices/AppointmentManagerImpl";
 import { ConcernClassification } from "../CoreServices/ConcernClassificationImpl";
-import { Service } from "../CoreServices/Service";
+import { EnergyTracker } from "../CoreServices/EnergyTrackingImpl";
 import { FinanceManager } from "../CoreServices/FinanceManagerImpl";
 import { Reporter } from "../CoreServices/ReportingImpl";
 
+
 export class ServiceProvider { 
-     
-chosenService!: Service;
-    appointmentManager!: AppointmentManager;
-concernClassification!: ConcernClassification;
-energyTracker!: EnergyTracker;
-financeManager!: FinanceManager;
-reporter!: Reporter;
+    
+     public appointmentManager: AppointmentManager;
+     public concernClassification: ConcernClassification;
+     public energyTracker: EnergyTracker;
+     public financeManager: FinanceManager;
+     public reporting: Reporter;
 
+constructor(){
+    this.appointmentManager = new AppointmentManager();
+    this.concernClassification = new ConcernClassification();
+    this.energyTracker = new EnergyTracker();
+    this.financeManager = new FinanceManager();
+    this.reporting = new Reporter()
+     }
 
-    setService(service: string) {
-
-        if(service === 'Appointment'){
-            this.chosenService = this.appointmentManager;
-        }
-        if(service === 'ConcernClassification'){
-            this.chosenService = this.concernClassification;
-        }
-        if(service === 'FinanceManager'){ 
-            this.chosenService = this.financeManager;
-        }
-        if(service === 'Reporting'){
-            this.chosenService = this.reporter;
-        }
-    }
-
-    public getService(): Service {
-
-            return this.chosenService
-
-        }
 }
