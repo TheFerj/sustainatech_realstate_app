635a3312bdbc285b90926a83cff03a0bc3b9e685
Kyle Dominic Lomigo <91366618+KyleDom@users.noreply.github.com>
2023-05-29 09:01:09 +0800

    update core services

---
+++ app/CoreServices/CoreServices/FinanceManagerImpl.ts
@@ -0,0 +1,15 @@
+
+import { IFinanceManager } from "../iCoreServices/FinanceManager";
+
+export class FinanceManager implements IFinanceManager {
+
+    // attributes go here 
+
+    showExpenseHistory() {
+        //TODO
+    }
+
+    generateLedger() {
+        //TODO
+    }
+}
